a2b0bb9e3a0d2f3ac3b0e6806b7d5a0688887daa
simonLab <simon.labondance@gmail.com>
2017-07-20 16:00:33 +0100

    rebase with master #14

--- web/controllers/tripdatabase/http_client.ex
+++ web/controllers/tripdatabase/http_client.ex
@@ -3,13 +3,12 @@ defmodule Bep.Tripdatabase.HTTPClient do
   Wrapper functions for the Tripdatabase API
   """
   alias Poison.Parser
-
-  def search(query) do
   @key Application.get_env(:bep, :tripdatabase_key)
   @api_url "https://www.tripdatabase.com"
+
   def search(query) do
     url = "#{@api_url}/search/json?key=#{@key}&criteria=" <> URI.encode(query)
-    {:ok, res} = HTTPoison.get(url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
+    {:ok, res} = HTTPoison.get(url, [], [ssl: [{:versions, [:'tlsv1.2']}]])
     # the api return byte order mark: "﻿{\"total\":6767}"
     {:ok, _data} = Parser.parse String.slice(res.body, 1..-1)
   end
