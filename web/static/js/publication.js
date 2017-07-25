511985ba6e071ed554da5424390c9fa7f4a6cc70
simonLab <simon.labondance@gmail.com>
2017-07-25 12:07:06 +0100

    log ajax error + for loop instead of forEach, https://github.com/dwyl/best-evidence/pull/72#discussion_r129264796

--- web/static/js/publication.js
+++ web/static/js/publication.js
@@ -1,7 +1,8 @@
 module.exports = (function() {
   var publications = document.querySelectorAll(".publication");
 
-  publications.forEach(function(publication) {
+  for(var i = 0; i < publications.length; i++) {
+    var publication = publications[i]
     publication.addEventListener("click", function(e) {
       var data = {
         search_id: e.target.dataset.searchId,
@@ -14,11 +15,15 @@ module.exports = (function() {
       request.setRequestHeader("Content-type", "application/json");
 
       request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
             console.log(request.responseText);
+          } else {
+            console.log('STATUS: ', request.status, 'REQUEST: ', request);
+          }
         }
       }
       request.send(JSON.stringify(data));
     })
-  })
+  }
 })()
