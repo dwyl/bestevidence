3ddd0bbe229d314c51c65e235230070d5576a35e
simonLab <simon.labondance@gmail.com>
2017-07-26 11:52:51 +0100

    display correct colour and pyramid for each results, #75

--- web/views/search_view.ex
+++ web/views/search_view.ex
@@ -1,3 +1,24 @@
 defmodule Bep.SearchView do
   use Bep.Web, :view
+  def colour_evidence(type) do
+    cond do
+      Enum.member?([1, 4, 9, 10, 11, 16, 18, 34], type) -> "b--evidence-1"
+      type == 13 -> "b--evidence-2"
+      type == 2 -> "b--evidence-3"
+      Enum.member?([27, 14], type) -> "b--evidence-4"
+      Enum.member?([5, 8, 22, 29, 30, 35], type) -> "b--evidence-5"
+      true -> "b--white"
+    end
+  end
+
+  def pyramid_logo(type) do
+    cond do
+      Enum.member?([1, 4, 9, 10, 11, 16, 18, 34], type) -> "pyramid-grade1.svg"
+      type == 13 -> "pyramid-grade2.svg"
+      type == 2 -> "pyramid-grade3.svg"
+      Enum.member?([27, 14], type) -> "pyramid-grade4.svg"
+      Enum.member?([5, 8, 22, 29, 30, 35], type) -> "pyramid-grade5.svg"
+      true -> ""
+    end
+  end
 end
