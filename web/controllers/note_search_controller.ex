0bbc98e32292f0a9b38a13989d07419ad508acc5
simonLab <simon.labondance@gmail.com>
2017-08-10 16:17:53 +0100

    update note for search term, #22

--- web/controllers/note_search_controller.ex
+++ web/controllers/note_search_controller.ex
@@ -16,9 +16,30 @@ defmodule Bep.NoteSearchController do
         |> redirect(to: history_path(conn, :index))
       {:error, changeset} ->
         search = Repo.get!(Search, note_params["search_id"])
-        render(conn, "new.html", changeset: changeset, search: search)
+        render conn, "new.html", changeset: changeset, search: search
     end
     conn
     |> redirect(to: history_path(conn, :index))
+  end
+
+  def edit(conn, %{"id" => note_id, "search_id" => search_id}) do
+    note = Repo.get!(NoteSearch, note_id)
+    search = Repo.get!(Search, search_id)
+    changeset = NoteSearch.changeset(note)
+    render conn, "edit.html", changeset: changeset, search: search
+  end
+
+  def update(conn, %{"id" => note_id, "note_search" => note_params}) do
+    note = Repo.get!(NoteSearch, note_id)
+    changeset = NoteSearch.changeset(note, note_params)
+
+    case Repo.update(changeset) do
+      {:ok, note} ->
+        conn
+        |> redirect(to: history_path(conn, :index))
+      {:error, changeset} ->
+        search = Repo.get!(Search, note_params["search_id"])
+        render conn, "edit.html", changeset: changeset, search: search
+    end
   end
 end
