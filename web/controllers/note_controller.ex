d7e48df0a6d843b51047ff0b89957a8dd3c1186f
Cleop <cleo.pearson@hotmail.co.uk>
2017-08-14 15:49:15 +0100

    Lists searches for notes page #22

--- web/controllers/note_controller.ex
+++ web/controllers/note_controller.ex
@@ -1,7 +1,35 @@
 defmodule Bep.NoteController do
   use Bep.Web, :controller
+  alias Bep.{User, Search, Publication, NoteSearch}
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  defp get_all_notes(u) do
+    User
+    |> Repo.get!(u.id)
+    |> Repo.preload(
+      searches: from(s in Search, order_by: [desc: s.inserted_at])
+    )
+    |> Repo.preload(
+      searches: [
+        publications: from(p in Publication, order_by: [desc: p.inserted_at])
+      ]
+    )
+    |> Repo.preload(
+      searches: :note_searches
+    )
+  end
+
+  def index(conn, _) do
+    user = conn.assigns.current_user
+    searches = get_all_notes(user).searches
+    |> group_searches_by_day()
+    render conn, "index.html", searches: searches
+  end
+
+  defp group_searches_by_day(searches) do
+    searches
+    |> Enum.group_by(
+      fn(s) -> Date.to_string(s.inserted_at)
+    end)
+    |> Enum.sort(fn({k1, _}, {k2, _}) -> k1 >= k2 end)
   end
 end
--- web/views/note_view.ex
+++ web/views/note_view.ex
@@ -1,3 +1,20 @@
 defmodule Bep.NoteView do
   use Bep.Web, :view
+
+  @doc"""
+    iex>format_date("2016-08-08")
+    "08-08-2016"
+    iex>format_date(Timex.format!(Timex.today, "{YYYY}-{0M}-{0D}"))
+    "Today"
+  """
+  def format_date(date) do
+    d = Date.from_iso8601!(date)
+    today = Timex.today
+    yesterday = Timex.shift(today, days: -1)
+    cond do
+      today == d -> "Today"
+      yesterday == d -> "Yesterday"
+      true -> Timex.format!(d, "{0D}-{0M}-{YYYY}")
+    end
+  end
 end
