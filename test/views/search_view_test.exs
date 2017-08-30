2286483628a0147c07814fd3227964df84a9325d
simonLab <simon.labondance@gmail.com>
2017-08-30 10:56:58 +0100

    add test for search_view

---
+++ test/views/search_view_test.exs
@@ -0,0 +1,4 @@
+defmodule Bep.SearchViewTest do
+  use Bep.ConnCase, async: true
+  doctest Bep.SearchView, import: true
+end
--- web/views/search_view.ex
+++ web/views/search_view.ex
@@ -26,5 +26,11 @@ defmodule Bep.SearchView do
  defp colour_evidence_format(int) when int == 0, do: "b--white"
  defp colour_evidence_format(int), do: "b--evidence-#{int}"
 
+ @doc"""
+   iex>colour_evidence(0)
+   "b--white"
+   iex>colour_evidence(22)
+   "b--evidence-5"
+ """
  def colour_evidence(type), do: format_class(type, &colour_evidence_format/1)
 end
