01b527401a1c0f2bebf5d8c62a1f0979f4b6db5e
simonLab <simon.labondance@gmail.com>
2017-09-01 17:08:59 +0100

    display publication on note page, #22

--- web/views/note_view.ex
+++ web/views/note_view.ex
@@ -2,4 +2,14 @@ defmodule Bep.NoteView do
   use Bep.Web, :view
 
   defdelegate format_date(date), to: Bep.HistoryView, as: :format_date
+
+  def has_note_publications?(search) do
+    if Enum.empty?(search.publications) do
+      false
+    else
+      Enum.reduce(search.publications, false, fn(p, acc) ->
+        acc || !Enum.empty?(p.note_publications)
+      end)
+    end
+  end
 end
