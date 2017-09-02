13ba4515d5b4be8c1335075aee0a36c59542d440
simonLab <simon.labondance@gmail.com>
2017-09-02 08:54:13 +0100

    add test for note page, #22

--- web/views/note_view.ex
+++ web/views/note_view.ex
@@ -3,6 +3,14 @@ defmodule Bep.NoteView do
 
   defdelegate format_date(date), to: Bep.HistoryView, as: :format_date
 
+  @doc"""
+    iex> s = %{publications: []}
+    iex>has_note_publications?(s)
+    false
+    iex> s = %{publications: [%{note_publications: [%{note: "note"}]}]}
+    iex>has_note_publications?(s)
+    true
+  """
   def has_note_publications?(search) do
     if Enum.empty?(search.publications) do
       false
