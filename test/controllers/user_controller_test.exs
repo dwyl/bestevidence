ec49ea56b9ffc08dc8a86f78454e858c08c7a671
robstallion <robertefrancis18@gmail.com>
2018-03-25 13:31:12 +0100

    update tests
    remove package-lock
    
    update travis yml
    
    update travis yml
    
    update travis yml

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -22,4 +22,30 @@ defmodule Bep.UserControllerTest do
     conn = put conn, user_path(conn, :update, user, %{"types": %{"1": "true", "2": "true"}})
     assert redirected_to(conn) == page_path(conn, :index)
   end
+
+  test "GET /users/new", %{conn: conn} do
+    conn = get conn, user_path(conn, :new)
+    assert html_response(conn, 200) =~ "Great. Let's get started."
+  end
+
+  test "POST /users/create redirect to /login when email already linked to an account", %{conn: conn} do
+    insert_user()
+    conn = post conn, user_path(conn, :create, %{"user" => %{"email": "email@example.com", "password": "supersecret"}})
+    assert html_response(conn, 302)
+  end
+
+  test "POST /users/create show /user/new when password is too short", %{conn: conn} do
+    insert_user()
+    conn = post conn, user_path(conn, :create, %{"user" => %{"email": "new@example.com", "password": "1"}})
+    assert html_response(conn, 200)
+  end
+
+  test "POST /users/create", %{conn: conn} do
+    insert_types()
+    conn = post conn, user_path(conn, :create, %{"user" => %{
+      "email": "new-email@example.com",
+      "password": "supersecret",
+      "1": "true"}})
+    assert html_response(conn, 302)
+  end
 end
