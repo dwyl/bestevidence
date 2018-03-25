531501d4c8d07df0436560437da93ebffb71c29e
robstallion <robertefrancis18@gmail.com>
2018-03-25 13:31:12 +0100

    update tests

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -1,29 +1,17 @@
 defmodule Bep.SessionControllerTest do
   use Bep.ConnCase
 
-  test "GET /users/new", %{conn: conn} do
-    conn = get conn, user_path(conn, :new)
-    assert html_response(conn, 200) =~ "Great. Let's get started."
+  test "GET /sessions/new", %{conn: conn} do
+    conn = get conn, session_path(conn, :new)
+    assert html_response(conn, 200) =~ "If you are on a public computer, remember to log out."
   end
 
-  test "POST /users/create redirect to /login when email already linked to an account", %{conn: conn} do
-    insert_user()
-    conn = post conn, user_path(conn, :create, %{"user" => %{"email": "email@example.com", "password": "supersecret"}})
-    assert html_response(conn, 302)
-  end
-
-  test "POST /users/create show /user/new when password is too short", %{conn: conn} do
-    insert_user()
-    conn = post conn, user_path(conn, :create, %{"user" => %{"email": "new@example.com", "password": "1"}})
+  test "POST /sessions/new", %{conn: conn} do
+    session =
+      %{"session" =>
+        %{"email" => "email@example.com", "password" => "password"}
+      }
+    conn = post conn, session_path(conn, :create, session)
     assert html_response(conn, 200)
-  end
-
-  test "POST /users/create", %{conn: conn} do
-    insert_types()
-    conn = post conn, user_path(conn, :create, %{"user" => %{
-      "email": "new-email@example.com",
-      "password": "supersecret",
-      "1": "true"}})
-    assert html_response(conn, 302)
   end
 end
