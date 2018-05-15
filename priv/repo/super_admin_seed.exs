8fc27a2a031e74d2393a46351181db248967aa17
robstallion <robertefrancis18@gmail.com>
2018-05-15 15:49:40 +0100

    update super admin seed file #284

--- priv/repo/super_admin_seed.exs
+++ priv/repo/super_admin_seed.exs
@@ -1,10 +1,14 @@
-alias Bep.{Repo, User}
+alias Bep.{Repo, Type, User}
+alias Ecto.Changeset
 
 changes = %{
   email: "super@admin.com",
   password: "password",
 }
 
+super_type = Repo.insert!(%Type{type: "super-admin"})
+
 %User{}
 |> User.registration_changeset(changes)
+|> Changeset.put_assoc(:types, [super_type])
 |> Repo.insert!()
