c0489ba6ae490bfb6633c78ca9e5117da078a7b0
robstallion <robertefrancis18@gmail.com>
2018-05-17 14:12:20 +0100

    create slug link helper to direct users to correct page #289 #20

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,10 +5,10 @@ defmodule Bep.PageController do
     if conn.assigns.current_user do
       redirect(conn, to: search_path(conn, :index))
     else
-      if Map.has_key?(conn.assigns, :client) do 
+      if Map.has_key?(conn.assigns, :client) do
         bg_colour = conn.assigns.client.login_page_bg_colour
         render(conn, "index.html", bg_colour: bg_colour)
-      else 
+      else
         default_colour = "#8f182e"
         render(conn, "index.html", bg_colour: default_colour)
       end
--- web/views/component_helpers.ex
+++ web/views/component_helpers.ex
@@ -2,10 +2,21 @@ defmodule Bep.ComponentHelpers do
   @moduledoc """
   Renders all of the component files
   """
-
+  use Phoenix.HTML
   alias Bep.ComponentView
 
   def component(template, assigns) do
     ComponentView.render "#{template}.html", assigns
+  end
+
+  def slug_link_helper(conn, str, f1, f2, route, classes) do
+    if Map.has_key?(conn.assigns, :client) do
+      link(str, [
+        to: f1.(conn, route, conn.assigns.client.slug),
+        class: classes
+      ])
+    else
+      link(str, [to: f2.(conn, route), class: classes])
+    end
   end
 end
