1cf4bad106bd332521006697e49920adb01206e2
robstallion <robertefrancis18@gmail.com>
2018-05-21 14:18:55 +0100

    update about colours #289

--- web/controllers/about_controller.ex
+++ web/controllers/about_controller.ex
@@ -2,6 +2,8 @@ defmodule Bep.AboutController do
   use Bep.Web, :controller
 
   def index(conn, _params) do
-      render conn, "index.html"
+      header_colour = get_client_colour(conn, :search_bar_colour)
+
+      render(conn, "index.html", header_colour: header_colour)
   end
 end
