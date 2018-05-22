32cbb6243b2b62071f1ba2b00661410cee96fb20
robstallion <robertefrancis18@gmail.com>
2018-05-22 14:23:04 +0100

    update postgrex to be compatible with heroku update

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Bep.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:postgrex, "~> 0.13.5"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
