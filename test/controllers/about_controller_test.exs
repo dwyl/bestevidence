c6ae0e0559aae4c51948bb41fa6f93477614b819
robstallion <robertefrancis18@gmail.com>
2018-05-23 09:33:28 +0100

    updates how about template serves about text #303

--- test/controllers/about_controller_test.exs
+++ test/controllers/about_controller_test.exs
@@ -1,9 +1,18 @@
 defmodule Bep.AboutControllerTest do
   use Bep.ConnCase
+  alias Plug.Conn
 
   test "GET /about", %{conn: conn} do
     conn = get conn, "/about"
     assert html_response(conn, 200) =~ "About BestEvidence"
   end
 
+  test "GET /about with client", %{conn: conn} do
+    conn =
+      conn
+      |> Conn.assign(:client, %{slug: "test", about_text: ""})
+      |> get("/about")
+
+    assert html_response(conn, 200) =~ "About BestEvidence"
+  end
 end
--- web/controllers/about_controller.ex
+++ web/controllers/about_controller.ex
@@ -1,7 +1,17 @@
 defmodule Bep.AboutController do
   use Bep.Web, :controller
+  alias Bep.{Client, Repo}
 
   def index(conn, _params) do
-      render conn, "index.html"
+    about_text =
+      if Map.has_key?(conn.assigns, :client) do
+        Map.get(conn.assigns.client, :about_text)
+      else
+        Client
+        |> Repo.get_by(name: "default")
+        |> Map.get(:about_text)
+      end
+
+    render(conn, "index.html", about_text: about_text)
   end
 end
