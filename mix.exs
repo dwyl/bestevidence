0704401ada4b4d1b277ec8c9f9d73985f252ee79
robstallion <robertefrancis18@gmail.com>
2018-05-24 08:36:27 +0100

    add new packages to project for s3 upload #303

--- mix.exs
+++ mix.exs
@@ -35,23 +35,28 @@ defmodule Bep.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, "~> 0.13.5"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.11.1"},
-     {:excoveralls, "~> 0.7.1"},
-     {:comeonin, "~> 3.2"},
-     {:credo, "~> 0.8.4", only: [:dev, :test], runtime: false},
-     {:timex, "~> 3.0"},
-     {:bamboo, "~> 0.7"},
-     {:bamboo_smtp, "~> 1.2.1"},
-     {:mock, "~> 0.2.0", only: :test}
-   ]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, "~> 0.13.5"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:httpoison, "~> 0.11.1"},
+      {:excoveralls, "~> 0.7.1"},
+      {:comeonin, "~> 3.2"},
+      {:credo, "~> 0.8.4", only: [:dev, :test], runtime: false},
+      {:timex, "~> 3.0"},
+      {:bamboo, "~> 0.7"},
+      {:bamboo_smtp, "~> 1.2.1"},
+      {:mock, "~> 0.2.0", only: :test},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_s3, "~> 2.0"},
+      {:sweet_xml, "~> 0.6"},
+      {:uuid, "~> 1.1"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
