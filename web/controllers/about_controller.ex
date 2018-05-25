be284fa2943226dac6820deb589ddbd1b01f3888
robstallion <robertefrancis18@gmail.com>
2018-05-25 17:42:27 +0100

    update about controller

--- web/controllers/about_controller.ex
+++ web/controllers/about_controller.ex
@@ -4,21 +4,8 @@ defmodule Bep.AboutController do
 
   def index(conn, _params) do
     bg_colour = get_client_colour(conn, :login_page_bg_colour)
-    about_text =
-      if Map.has_key?(conn.assigns, :client) do
-        Map.get(conn.assigns.client, :about_text)
-      else
-        Client
-        |> Repo.get_by(name: "default")
-        |> Map.get(:about_text)
-      end
-
-    logo_url =
-      if Map.has_key?(conn.assigns, :client) do
-        Map.get(conn.assigns.client, :logo_url)
-      else
-        "default"
-      end
+    about_text = conn.assigns.client.about_text
+    logo_url = conn.assigns.client.logo_url
 
     render(
       conn,
