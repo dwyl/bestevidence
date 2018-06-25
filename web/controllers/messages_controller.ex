b89e5caeda6b104fb437e61df971dd0fba178f58
Robert Francis <robertefrancis18@gmail.com>
2018-06-25 16:12:29 +0100

    display messages that are specific to that user #339

--- web/controllers/messages_controller.ex
+++ web/controllers/messages_controller.ex
@@ -1,8 +1,21 @@
 defmodule Bep.MessagesController do
   use Bep.Web, :controller
+  alias Bep.{Messages, Repo}
 
   def view_messages(conn, _params) do
-    assigns = [messages: ["1", "2"]]
+    query = create_query(conn)
+    messages = Repo.all(query)
+    assigns = [messages: messages]
     render(conn, :view, assigns)
+  end
+
+  def create_query(conn) do
+    client_id = conn.assigns.client.id
+    current_user_id = conn.assigns.current_user.id
+
+    from m in Messages,
+    where: m.to_all == true
+    or m.to_client == ^client_id
+    or m.to_user == ^current_user_id
   end
 end
