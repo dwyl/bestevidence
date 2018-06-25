bc0f96db109144ba403e062b39f2ed0780a78d1e
Robert Francis <robertefrancis18@gmail.com>
2018-06-25 16:11:20 +0100

    create migration and schema for messages to users #339

---
+++ web/models/messages.ex
@@ -0,0 +1,14 @@
+defmodule Bep.Messages do
+  use Bep.Web, :model
+
+  @moduledoc false
+
+  schema "messages" do
+    field :subject, :string
+    field :body, :string
+    field :to_all, :boolean
+    field :to_client, :id
+    field :to_user, :id
+    field :from_id, :id
+  end
+end
