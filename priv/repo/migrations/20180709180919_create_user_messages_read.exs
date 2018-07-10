34f9b079fc1744a7bab4f565bfbc093bd296fc1d
Robert Francis <robertefrancis18@gmail.com>
2018-07-10 14:27:13 +0100

    Create user_message_read migration and schemas #355

---
+++ priv/repo/migrations/20180709180919_create_user_messages_read.exs
@@ -0,0 +1,40 @@
+defmodule Bep.Repo.Migrations.CreateUserMessagesRead do
+  use Ecto.Migration
+  alias Bep.{Repo, User, UserMessagesRead}
+
+  def up do
+    create table(:user_messages_read, primary_key: false) do
+      add :user_id, references(:users, on_delete: :delete_all), primary_key: true
+      add :messages_read_time, :utc_datetime
+      add :message_received_at, :utc_datetime
+    end
+    create unique_index(:user_messages_read, [:user_id])
+
+    flush()
+
+    date_time_now = DateTime.utc_now()
+
+    # can this be done in a more efficient way? See below
+    User
+    |> Repo.all()
+    |> Enum.map(fn(user) ->
+      Repo.insert(
+        %UserMessagesRead{
+          user_id: user.id,
+          messages_read_time: date_time_now,
+          message_received_at: date_time_now
+        }
+      )
+    end)
+    # INSERT INTO user_messages_read
+    #   (user_id)
+    # VALUES
+    #   (
+    #     SELECT user_id FROM ...
+    #   )
+  end
+
+  def down do
+    drop table(:user_messages_read)
+  end
+end
---
+++ web/models/user_messages_read.ex
@@ -0,0 +1,34 @@
+defmodule Bep.UserMessagesRead do
+  @moduledoc """
+  UserMessagesRead model
+  """
+
+  use Bep.Web, :model
+  alias Bep.{Repo, User, UserMessagesRead}
+
+  @primary_key false
+  schema "user_messages_read" do
+    belongs_to :user, User, primary_key: true
+    field :messages_read_time, :utc_datetime
+    field :message_received_at, :utc_datetime
+  end
+
+  def new_msg_changeset(struct, params \\ %{}) do
+    date_time_now = DateTime.utc_now()
+
+    struct
+    |> cast(params, [:user_id, :message_received_at])
+    |> validate_required([:user_id, :message_received_at])
+    |> put_change(:message_received_at, date_time_now)
+  end
+
+  # Helpers
+  def update_user_msg_read(message) do
+    if is_integer(message.to_user) do
+      UserMessagesRead
+      |> Repo.get(message.to_user)
+      |> UserMessagesRead.new_msg_changeset()
+      |> Repo.update!
+    end
+  end
+end
