f99a74187c95598051c7613656d5f40017b396ac
Robert Francis <robertefrancis18@gmail.com>
2018-07-20 10:33:15 +0100

    creates a questions table #266

--- web/controllers/note_controller.ex
+++ web/controllers/note_controller.ex
@@ -28,8 +28,9 @@ defmodule Bep.NoteController do
 
   def index(conn, _) do
     user = conn.assigns.current_user
-    searches = get_all_notes(user).searches
-    |> Search.group_searches_by_day()
-    render conn, "index.html", searches: searches
+    searches =
+      get_all_notes(user).searches
+      |> Search.group_searches_by_day()
+    render(conn, "index.html", searches: searches)
   end
 end
