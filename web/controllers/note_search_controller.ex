38a192055904057d7832474cee8f1743fca966b7
Robert Francis <robertefrancis18@gmail.com>
2018-07-21 14:52:06 +0100

    update notes page so that a user can start creating a pico #371 #372

--- web/controllers/note_search_controller.ex
+++ web/controllers/note_search_controller.ex
@@ -5,22 +5,26 @@ defmodule Bep.NoteSearchController do
   def new(conn, params) do
     search = Repo.get!(Search, params["search_id"])
     changeset = NoteSearch.changeset(%NoteSearch{})
-    btn_colour = get_client_colour(conn, :btn_colour)
-
-    render(
-      conn,
-      "new.html",
+    assigns = [
       changeset: changeset,
       search: search,
-      btn_colour: btn_colour
-    )
+      btn_colour: get_client_colour(conn, :btn_colour)
+    ]
+    render(conn, "new.html", assigns)
   end
 
+  # starting a pico
+  def create(conn, %{"note_search" => note_params, "start_pico" => "true"}) do
+    changeset = NoteSearch.changeset(%NoteSearch{}, note_params)
+    note = Repo.insert!(changeset)
+    redirect(conn, to: pico_search_path(conn, :new, note_id: note.id))
+  end
+
+  # this will become where the save and continue later btn will go
   def create(conn, %{"note_search" => note_params}) do
     changeset = NoteSearch.changeset(%NoteSearch{}, note_params)
     Repo.insert!(changeset)
-    conn
-    |> redirect(to: history_path(conn, :index))
+    redirect(conn, to: history_path(conn, :index))
   end
 
   def edit(conn, %{"id" => note_id, "search_id" => search_id}) do
