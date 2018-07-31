3bb59da0d6156902a7cf0d266687b801c550a251
Robert Francis <robertefrancis18@gmail.com>
2018-07-31 10:51:14 +0100

    update pico search create action based on comments in issue #373

--- web/controllers/pico_search_controller.ex
+++ web/controllers/pico_search_controller.ex
@@ -17,59 +17,40 @@ defmodule Bep.PicoSearchController do
     search = Repo.get(Search, search_id)
     pico_outcomes = get_pico_outcomes(pico_search_params)
     note_search = Repo.get(NoteSearch, note_id)
-    changeset =
+    pico_search =
       %PicoSearch{}
       |> PicoSearch.changeset(pico_search_params)
       |> Changeset.put_assoc(:note_search, note_search)
+      |> Repo.insert!()
 
-    case Repo.insert(changeset) do
-      {:ok, pico_search} ->
-        Enum.map(pico_outcomes, fn(outcome) ->
-          # MAKE THIS AN ISSUE
-          # Not blocker but something to look into
-          # I should only insert new outcomes or outcomes that have been updated
-          # Could add an extra frild to the outcome map which can say if it is
-          # a new outcome to be inserted or not e.g. outcome_new_bool: true
-          %PicoOutcome{}
-          |> PicoOutcome.changeset(outcome)
-          |> Changeset.put_assoc(:pico_search, pico_search)
-          |> Repo.insert!()
-        end)
+    Enum.each(pico_outcomes, fn(outcome) ->
+      %PicoOutcome{}
+      |> PicoOutcome.changeset(outcome)
+      |> Changeset.put_assoc(:pico_search, pico_search)
+      |> Repo.insert!()
+    end)
 
-        case Map.get(params, "search_trip") do
-          # Save and continue later route
-          nil ->
-            redirect(conn, to: search_path(conn, :index))
+    case Map.get(params, "search_trip") do
+      nil -> # Save and continue later route
+        redirect(conn, to: search_path(conn, :index))
+      "true" -> # search api with pico search (regular search for now)
+        user = conn.assigns.current_user
+        search_data =
+          %{"term" => search.term}
+          |> Search.search_data_for_create(user)
 
-          # make api call with the search term render the Search results.html
-          # this will become the pico search route but reg search for now
-          "true" ->
-            user = conn.assigns.current_user
-            search_data =
-              %{"term" => search.term}
-              |> Search.search_data_for_create(user)
+        search =
+          search
+          |> Changeset.change(number_results: search_data.data["total"])
+          |> Repo.update!(force: true)
 
-            search =
-              search
-              |> Changeset.change(number_results: search_data.data["total"])
-              |> Repo.update!(force: true)
-
-            assigns =
-              [
-                search: search,
-                data: search_data.data,
-                bg_colour: get_client_colour(conn, :login_page_bg_colour),
-                search_bar_colour: get_client_colour(conn, :search_bar_colour)
-              ]
-            render(conn, Bep.SearchView, "results.html", assigns)
-        end
-
-      # not sure if this case is needed. Awaiting question to be answered on
-      # gh https://git.io/fNzTD
-      {:error, changeset} ->
-        search = Repo.get(Search, search_id)
-        assigns = [changeset: changeset, note_id: note_id, search: search]
-        render(conn, "new.html", assigns)
+        assigns = [
+          search: search,
+          data: search_data.data,
+          bg_colour: get_client_colour(conn, :login_page_bg_colour),
+          search_bar_colour: get_client_colour(conn, :search_bar_colour)
+        ]
+        render(conn, Bep.SearchView, "results.html", assigns)
     end
   end
 
