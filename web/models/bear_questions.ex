0792f239e8aad37e7b24cd991e69959a9d2b6737
Robert Francis <robertefrancis18@gmail.com>
2018-08-16 14:59:03 +0100

    small text and styling changes

--- web/models/bear_questions.ex
+++ web/models/bear_questions.ex
@@ -39,7 +39,7 @@ defmodule Bep.BearQuestion do
   def check_validity_questions do
     [
       "Did the trial address a clearly focused issue?",
-      "Was the assignment of patients to treatments randomised?",
+      "Was the assignment of patients to treatments randomized?",
       "Were all of the patients who entered the trial properly accounted for at its conclusion?",
       "Were patients, health workers and study personnel 'blind' to treatment?",
       "Were the groups similar at the start of the trial?",
@@ -61,7 +61,7 @@ defmodule Bep.BearQuestion do
 
   def relevance_questions do
     [
-      "Can the results be applied to the local population, or in your context?",
+      "Can the results be applied to your patient or local population?",
       "Were all clinically important outcomes considered?",
       "Are the benefits worth the harms and costs?",
       "Posterior probability",
